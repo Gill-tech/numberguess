766a60a64948938b01b356e956c1fc407bb97afb
Brian Gills <101551262+Gill-tech@users.noreply.github.com>
2024-02-10 09:32:12 +0300

    Add files via upload

---
+++ numbgues.py
@@ -0,0 +1,20 @@
+import random 
+guesses=0
+top_of_range= input ("type a number: ")
+
+
+if top_of_range.isdigit():
+    top_of_range=int(top_of_range)
+
+    if top_of_range<=0:
+        print('please type a number larger than 0 next time')
+        quit()
+
+else:
+    print('please type a number next time .')
+    quit()
+
+
+random_number=random.randint(0, top_of_range)
+print(random_number)
+
